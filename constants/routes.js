8adfda54e6d4d8f6ca85f92ccb320f226a25ea0b
rachmathidayat <rachmathidayat.dev@gmail.com>
2021-03-10 14:24:50 +0700

    update

--- constants/routes.js
+++ constants/routes.js
@@ -4,65 +4,6 @@ const INDEX = {
 	url: '/',
 }
 
-const PRODUCT = {
-	PRODUCT_LIST: {
-		href: '/product/index.vue',
-		route: '/product',
-		url: '/product',
-	},
-	PRODUCT_DETAIL: {
-		href: '/product/detail.vue',
-		route: '/product/detail/:slug',
-		url: '/product/detail',
-	},
-	PRODUCT_SEARCH: {
-		href: '/product/search.vue',
-		route: '/product/search',
-		url: '/product/search',
-	},
-}
-
-const ORDER = {
-	CART: {
-		href: '/order/cart.vue',
-		route: '/order/cart',
-		url: '/order/cart',
-	},
-	CHECKOUT: {
-		href: '/order/checkout.vue',
-		route: '/order/delivery',
-		url: '/order/delivery',
-	},
-	ORDER_LIST: {
-		href: '/order/list.vue',
-		route: '/order',
-		url: '/order',
-	},
-}
-
-const FAVORIT = {
-	href: '/favorit.vue',
-	route: '/favorit',
-	url: '/favorit',
-}
-
-const PROFILE = {
-	href: '/profile.vue',
-	route: '/profile',
-	url: '/profile',
-}
-
-const HISTORY = {
-	href: '/history.vue',
-	route: '/history',
-	url: '/history',
-}
-
 module.exports = {
 	INDEX,
-	...PRODUCT,
-	...ORDER,
-	FAVORIT,
-	PROFILE,
-	HISTORY,
 }
--- views/containers/home/mobile/index.vue
+++ views/containers/home/mobile/index.vue
@@ -3,19 +3,24 @@
 		<Row>
 			<Column custom-class="header-container">
 				<Column custom-class="item-content">
-					<TextView custom-class="item-name font-28" font-weight="500"
+					<TextView
+						custom-class="item-name font-28"
+						font-weight="600"
+						font-color="tan"
 						>Radita</TextView
 					>
 					<TextView
 						custom-class="item-name font-48 custom-mt"
 						text-alignment="center"
 						font-weight="bold"
+						font-color="tan"
 						>&</TextView
 					>
 					<TextView
 						custom-class="item-name font-28 custom-mt"
 						text-alignment="right"
-						font-weight="500"
+						font-weight="600"
+						font-color="tan"
 						>Rachmat</TextView
 					>
 				</Column>
@@ -24,12 +29,24 @@
 					align-items="center"
 					justify-content="center"
 				>
-					<TextView custom-class="font-14">Yth</TextView>
-					<TextView custom-class="font-18">{{ guestName }}</TextView>
+					<TextView custom-class="font-14" font-color="tan" font-weight="bold"
+						>Yth</TextView
+					>
+					<TextView
+						custom-class="font-18"
+						font-color="tan"
+						font-weight="bold"
+						>{{ guestName }}</TextView
+					>
 					<Row>
 						<img :src="images.divider" width="180" />
 					</Row>
-					<TextView custom-class="font-14">( {{ guestLocation }} )</TextView>
+					<TextView custom-class="font-14" font-color="tan" font-weight="bold"
+						>( {{ guestLocation }} )</TextView
+					>
+					<button class="btn-undangan mt-20" type="button">
+						Buka Undangan
+					</button>
 				</Column>
 			</Column>
 		</Row>
