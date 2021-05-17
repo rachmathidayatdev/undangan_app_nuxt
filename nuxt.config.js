3eb532210acf2e498094a2a1ef160a5d74e79acf
rachmathidayat <rachmathidayat.dev@gmail.com>
2021-05-17 17:04:07 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ const ROUTES = require('./constants/routes')
 export default {
 	// Global page headers (https://go.nuxtjs.dev/config-head)
 	head: {
-		title: 'undangan_app_vue',
+		title: 'invitation_radita_rachmat',
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
